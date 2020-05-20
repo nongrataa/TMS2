69663094fa80ed27e15bddd90b8cc8dae4969a0f
Your Name <you@example.com>
2020-05-20 21:54:20 +0300

    Фактариал числа цикл до 1000 Площадь треугольника

---
+++ factorial.py
@@ -0,0 +1,6 @@
+a = int(input(" Число "))
+f = 1
+for i in range(1, a+1):
+    f = f*i
+
+print("f = ", f)
---
+++ while1000.py
@@ -0,0 +1,6 @@
+i = 0
+result=0
+while result < 1000:
+    result = i
+    print(result)
+    i += 1

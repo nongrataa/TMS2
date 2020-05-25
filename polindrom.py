bd637164143892050ca511707c931e9cb95d09b7
devAlexanderKonopelko <konopelko99@mail.ru>
2020-05-25 21:23:12 +0300

    сложение возраста

--- polindrom.py
+++ polindrom.py
@@ -1,15 +1,22 @@
 data = "sasha 43, masha 23"
-
+change_person = False
+counter = 0
 
 print(data.replace(',', '').split())
 
-
-sasha_age=''
+sasha_age = ''
+masha_age = ''
 
 for i in data.replace(',', '').split():
-
-    if i.isdigit():
-        sasha_age+=i
-
+    for j in i:
+        if j.isdigit() and change_person is False:
+            sasha_age += j
+            counter += 1
+            if counter >= 2:
+                change_person = True
+        elif j.isdigit() and change_person:
+            masha_age += j
 
 print(sasha_age)
+print(masha_age)
+print(int(sasha_age)+int(masha_age))

1a85f0ad3e1c919ef07b0328a4b53d8d517655d1
nongrataa <aliaksandr.hushcha@gmail.com>
2020-06-01 02:22:44 +0300

    Создание readme

--- Day2/task_2_5.py
+++ Day2/task_2_5.py
@@ -4,5 +4,5 @@ for i in range(len(string)):
     if i % 2 == 0:
         new_string += string[i]
 
-print("Ваша строка - ",string)
-print("Новая строка - ",new_string)
+print("Ваша строка - ", string)
+print("Новая строка - ", new_string)

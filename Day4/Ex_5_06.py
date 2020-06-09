1d4f967328ee817b087b006fb913a6e269fc4e23
nongrataa <aliaksandr.hushcha@gmail.com>
2020-06-09 20:20:47 +0300

    day 4

--- Day4/Ex_5_06.py
+++ Day4/Ex_5_06.py
@@ -1,5 +1,6 @@
 """
-Создать список учеников подобной структуры. Определить средний балл оценок по всем предметам, и вывести сведения о студентах, средний балл которых больше 4
+Создать список учеников подобной структуры. Определить средний балл оценок по всем предметам,
+и вывести сведения о студентах, средний балл которых больше 4
 pupils = [
   {
         'firstname': 'Masha',
@@ -23,7 +24,7 @@ puplist = [
         'Firstname': 'Polina',
         'Group': 31,
         'phyisics': 5,
-        'informatics': 4,
+        'informatics': 10,
         'history': 7,
     },
     {
@@ -35,21 +36,18 @@ puplist = [
     }
 ]
 
-sum = 0
-for i in puplist:
-    print(i)
-    for key, value in i.items():
-        # print(f'key - {key} value - {value}')
-        if key == 'phyisics':
-            print(value)
-            sum = sum + int(value)
-        if key == 'informatics':
-            print(value)
-            sum = sum + int(value)
-        if key == 'informatics':
-            print(value)
-            sum = sum + int(value)
+def inf_student(list):
+    for i in puplist:
+        sum = 0
+        average = 0
+        for key, value in i.items():
+            if key == 'phyisics' or key == 'informatics' or key == 'history':
+                sum = sum + int(value)
+        average = sum / 3
+        if average > 7:
+                print(f"Имя студента - {i['Firstname']} \nГруппа - {i['Group']} \nОценки по предметам: \nФизика - {i['phyisics']} \nИнформатика - {i['informatics']} \nИстория - {i['history']}")
+                print(f'Средний бал студента - {average} \n')
 
-    print(f' average - {sum/3}')
+    return
 
-print(sum)
+inf_student(puplist)

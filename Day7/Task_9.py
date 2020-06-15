dc9132fed1a545ae20cb0b6e6dae98a55bed6114
nongrataa <aliaksandr.hushcha@gmail.com>
2020-06-15 21:49:06 +0300

    day 7

--- Day7/Task_9.py
+++ Day7/Task_9.py
@@ -1,2 +1,5 @@
-f = lambda **kwargs:kwargs
-print(f(abc="1",abcabc="3",abcabcabc="4"))
+"""Создать lambda функцию, которая принимает на вход неопределенное количество именных
+аргументов и выводит словарь с ключами удвоенной длины. {‘abc’: 5} -> {‘abcabc’: 5}"""
+
+f = lambda **kwargs: kwargs
+print(f(abc="1", abcabc="3", abcabcabc="4"))

2e038df06295f224c47a6e9e0a577eac2f89806a
nongrataa <aliaksandr.hushcha@gmail.com>
2020-06-15 14:26:10 +0300

    day7

---
+++ Day7/Task_3.py
@@ -0,0 +1,8 @@
+"""Создать lambda функцию, которая принимает на вход список имен и выводит их в формате “Hello, {name}” в другой список"""
+my_list = ['Sasha', 'Masha', 'Dasha', 'Dima', 'Petya']
+
+my_list2 = [2, 4, 4, 6, 7]
+
+g = map(lambda x: 'Hello ' + x, my_list)
+
+print(list(g))
--- main.py
+++ main.py
@@ -1,25 +1,2 @@
-def change_before_diagana(matrix_a, matrix_b):
-    index = 0
-    for i in range(len(matrix_a)):
-        for j in range(len(matrix_a[i])):
-            if i > index and j <= index + 1:
-                matrix_a[i][j], matrix_b[i][j] = matrix_b[i][j], matrix_a[i][j]
-            if i <= index and j >= index + 1:
-                print(matrix_a[i][j])
-                matrix_a[i][j], matrix_b[i][j] = matrix_b[i][j], matrix_a[i][j]
-
-    return matrix_b
-
-
-a = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 2]
-]
-b = [
-    [5, 6, 2],
-    [1, 7, 4],
-    [9, 3, 7]
-]
-
-print(change_before_diagana(a, b))
+g = lambda x: x * 2
+print(g(2))

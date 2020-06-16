6467b55a771a2b7cbb29d540369a71a973146d32
nongrataa <aliaksandr.hushcha@gmail.com>
2020-06-16 23:21:45 +0300

    day 8 Task_3

---
+++ Day8/Task_3.py
@@ -0,0 +1,18 @@
+
+def write_file(file, string):
+    with open(file, 'a') as my_file:
+        for line in string:
+            my_file.write(line + '\n')
+    return
+
+
+def enter_string():
+    my_list = []
+    for i in range(0, 3):
+        string = str(input(f'Введите строку - {i+1} - '))
+        my_list.append(string)
+    write_file(file, my_list)
+
+
+file = 'test.txt'
+enter_string()

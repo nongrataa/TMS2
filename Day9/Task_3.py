e6b89b82afa8ef4d6390ba53541cca9dd361a7d1
nongrataa <aliaksandr.hushcha@gmail.com>
2020-06-22 11:56:12 +0300

    day9

---
+++ Day9/Task_3.py
@@ -0,0 +1,13 @@
+"""Добавить два метода в класс Dog: jump и run. Методы выводят на экран Jump! и Run! соответственно."""
+class dog():
+
+    def jump(self):
+        print("Бобик")
+
+    def run(self):
+        print("Песик")
+
+a = dog()
+
+a.jump()
+a.run()
